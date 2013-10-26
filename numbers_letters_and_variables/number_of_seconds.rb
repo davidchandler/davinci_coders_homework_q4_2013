3bdcfd2440384f29359b226e971945c0bb60ce93
David Chandler <dchandler43@hotmail.com>
2013-10-26 00:04:33 -0600

    number of seconds.rb

--- numbers_letters_and_variables/number_of_seconds.rb
+++ numbers_letters_and_variables/number_of_seconds.rb
@@ -8,7 +8,7 @@ number_of_weeks_in_year = 52
 
 number_of_seconds_in_hour = number_of_seconds_in_minute * number_of_minutes_in_hour
 
-number_of_seconds_in_day = number_of_seconds_in_minute * number_of_minutes_in_hour * number_of_hours_in_day
+number_of_seconds_in_day  = number_of_seconds_in_minute * number_of_minutes_in_hour * number_of_hours_in_day
 
 number_of_seconds_in_week = number_of_seconds_in_minute * number_of_minutes_in_hour * number_of_hours_in_day * number_of_days_in_week
 
@@ -30,9 +30,9 @@ puts "There are #{number_of_days_in_week} days in a week"
 
 puts "That means there are:"
 
-puts " #{number_of_seconds_in_hour} seconds in an hour,"
-puts " #{number_of_seconds_in_day} seconds in a day,"
-puts " #{number_of_seconds_in_week} seconds in a week"
+puts "  #{number_of_seconds_in_hour} seconds in an hour,"
+puts "  #{number_of_seconds_in_day} seconds in a day,"
+puts "  #{number_of_seconds_in_week} seconds in a week"
 
 puts "That means when you turn 20, you've been alive for #{number_of_seconds_at_age_20} seconds,"
 puts "and if you make it to 100, you will have lived #{number_of_seconds_at_age_100} seconds. Make them count!"
