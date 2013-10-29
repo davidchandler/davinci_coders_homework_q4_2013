feb45ee36286cb9a399b50d7b7b1dec30d3c4081
David Chandler <dchandler43@hotmail.com>
2013-10-28 21:49:07 -0600

    Update array hw

--- array_practice.rb
+++ array_practice.rb
@@ -2,33 +2,31 @@
 
 myarray = [1,2,3,4,5,6,7,8,9,10]
 
-myarray.each { |a| print a , "..." }
+   #puts "1...2...3...4...5...6...7...8...9...10..."
+puts "#{myarray.join('...')}..."
 
-#puts "T-" + myarray.reverse.each { |a| print a , "..." }.to_s + " BLASTOFF!-"
+  # puts "T-10, 9, 8, 7, 6, 5, 4, 3, 2, 1...  BLASTOFF!"
+puts "T-#{myarray.reverse.join(', ')}...  BLASTOFF!"  
+  
+puts "The last element is #{myarray.last}" 
+  
+puts "The first element is #{myarray.first}" 
+   
+puts "The third element is #{myarray[2]}" 
+   
+puts "The element with an index of 3 is #{myarray[3]}" 
+   
+puts "The second from last element is #{myarray[-2]}" 
+   
+puts "The first four elements are '#{myarray[0..3].join(', ')}'"
+   
+myarray -= [5, 6, 7]
+puts "If we delete 5, 6 and 7 from the array, we're left with " + myarray.to_s.gsub(/\s+/, "")
+ 
+puts "If we add 5 at the beginning of the array, we're left with [#{myarray.unshift(5).join(',')}]" 
 
-#myarray.each do |a|
- # puts a + ","
-#end
+puts "If we add 6 at the end of the array, we're left with [#{myarray.push(6).join(',')}]" 
+   
+puts "Only the elements #{myarray.sort[7,8]} are > 8." 
 
-puts "The last element is " + myarray.last.to_s
-
-puts "The first element is " + myarray.first.to_s
-
-puts "The third element is " + myarray[2].to_s
-puts "The element with an index of 3 is " + myarray[3].to_s
-puts "The second from last element is " + myarray[-2].to_s
-puts "The first four elements are " + "'" + myarray[0..3].join(",") + "'"
-
-
-puts "If we delete 5,6 and 7 from the array, we're left with " 
-
-arraybegin = myarray.unshift(5)
-puts "If we add 5 at the beginning of the array, we're left with "  + arraybegin.to_s
-
-arrayend = myarray << 6
-puts "if we add 6 at then end of the array, we're left with " + arrayend.to_s
-
-puts "Only the elements, " + myarray.sort[10,11].to_s  + " are > 8" 
-
-puts "If we remove all of the elements, then the length of the array is " + myarray.clear.length.to_s
-
+puts "If we remove all the elements, then the length of the array is #{myarray.clear.length}" 
